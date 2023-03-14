6c87df3c413ca29df046f1995553687efff9730e
Ryan Santiago <cr7rs7@gmail.com>
2023-03-13 23:54:51 -0400

    Add functionality to prompt. Add functionality to guesses input. Track guess values and attempts.

--- index.js
+++ index.js
@@ -1,24 +1,49 @@
+// Array to contain guess values
+const guesses = []
+// Variable to track amount of guesses
+let tries = 1
+// ID for guess range text
 let guess_range = document.getElementById('guess-range')
+// prompt for 
+let range = Math.round(prompt('Enter your max number here.', '1'))
 
-function do_range() {
-    let range = prompt('Enter your max number here.', '0')
-    
-    if(range != null) {
-        guess_range.innerHTML = `Your guess range is between 1 and ${range}, good luck!`
-    }
+if (isNaN(range) || range <= 1) {
+    location.reload()
+} else {
+    guess_range.innerHTML = `Your guess range is between 1 and ${range}, good luck!` 
 }
 
-let num = Math.floor(Math.random() * 20) + 1
+
+let num = Math.floor(Math.random() * range) + 1
 
 function do_guess() {
     let guess = Number(document.getElementById('guess').value)
     let message = document.getElementById('message')
 
-    if (num == guess) {
-        message.innerHTML = 'You got it! Good guess!'
-    } else if (num > guess) {
-        message.innerHTML = `Your guess was too low. Try again!`
+    if(isNaN(guess)) {
+        alert('That is not a number.')
+    } else if (guess > range || guess < 1) {
+        alert('That number is outside of the range, try again!')
     } else {
-        message.innerHTML = `Your guess was too high. Try again!`
-    }
+        if (num == guess) {
+            message.innerHTML = `You got it in ${tries} tries! Good guess!`
+            tries++
+            guesses.push(guess)
+        } else if (num > guess) {
+            message.innerHTML = `Your guess was too low. Try again!`
+            tries++
+            if(guesses.includes(guess)) {
+                alert('You\'ve already guessed this number.')
+            } else guesses.push(guess)
+        } else {
+            message.innerHTML = `Your guess was too high. Try again!`
+            tries++
+            if(guesses.includes(guess)) {
+                alert('You\'ve already guessed this number.')
+            } else guesses.push(guess)
+        }
+    } 
 }
+
+
+
